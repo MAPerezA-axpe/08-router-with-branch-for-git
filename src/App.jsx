64eae01ca125bed2ceb02de230b7b38a05421af4
Miguel Angel Perez <MAPerezA@uxcale.com>
2024-09-17 09:59:44 +0200

    Third commit: no recargar toda la página

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate (href) {
+  window.history.pushState({}, '', href)
+  //Creamos un evento personalizado para avisar de que hemos cambiado la URL
+  const navigationEvent = new Event('pushstate')
+  window.dispatchEvent(navigationEvent)
+}
 
 function HomePage () {
   return (
     <>
       <h1>HOME</h1>
       <p>Esta es la página para crear un React Router desde cero</p>
-      <a href='/about'>Ir a Sobre nosotros</a>
+      <button onClick={() => navigate('/about')}>Ir a Sobre nosotros</button>
     </>
   )
 }
@@ -19,13 +28,25 @@ function AboutPage () {
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKJd5EPOWibW59EuALDpuzpnCRLhHfDp0udw&s' alt='Foto de perfil de Miguel Ángel' />
         <p>¡Hola! Soy Miguel Ángel y esto es un clon de react router</p>
       </div>
-      <a href='/'>Ir a la página principal</a>
+      <button onClick={() => navigate('/')}>Ir a la página principal</button>
     </>
   )
 }
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, []) //El useEffect solo debe ejecutarse la primera vez
 
   return (
     <>
