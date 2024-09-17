1334681a91a41312dfe1e139cf33b0b275bd9c9e
Miguel Angel Perez <MAPerezA@uxcale.com>
2024-09-17 09:38:38 +0200

    Second commit: home and about are now separated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+function HomePage () {
+  return (
+    <>
+      <h1>HOME</h1>
+      <p>Esta es la página para crear un React Router desde cero</p>
+      <a href='/about'>Ir a Sobre nosotros</a>
+    </>
+  )
+}
+
+function AboutPage () {
+  return (
+    <>
+      <h1>ABOUT</h1>
+      <div>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKJd5EPOWibW59EuALDpuzpnCRLhHfDp0udw&s' alt='Foto de perfil de Miguel Ángel' />
+        <p>¡Hola! Soy Miguel Ángel y esto es un clon de react router</p>
+      </div>
+      <a href='/'>Ir a la página principal</a>
+    </>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
   return (
     <>
-      <h1>React Router</h1>
+      <main>
+        {currentPath === '/' && <HomePage />}
+        {currentPath === '/about' && <AboutPage />}
+      </main>
     </>
   )
 }
